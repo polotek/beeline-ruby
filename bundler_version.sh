65c3350127e8b469971a9ac7e0dc3657bd710ac8
Martin Holman <me@martinholman.co.nz>
2019-10-04 08:32:08 -0700

    Update bundle version for rails 4

--- bundler_version.sh
+++ bundler_version.sh
@@ -2,7 +2,7 @@
 
 set -ux
 
-if [[ "$BUNDLE_GEMFILE" =~ (rails_41.gemfile|rails_42.gemfile)$ ]]; then
+if [[ "$BUNDLE_GEMFILE" =~ (rails_4.gemfile|rails_41.gemfile|rails_42.gemfile)$ ]]; then
   gem uninstall -v '>= 2' -i $(rvm gemdir) -ax bundler
   gem uninstall -v '>= 2' -i $(rvm gemdir)@global -ax bundler
   gem install bundler -v '< 2'
